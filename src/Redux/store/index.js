48f335e8fa9cc28eb7c09b07a59056f1afd16861
Amir B. Kahna <amir.kahna2@gmail.com>
2026-05-17 01:51:17 +0200

    Fixed reducers path

--- src/Redux/store/index.js
+++ src/Redux/store/index.js
@@ -1,13 +1,7 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+
 import favouritesReducer from "../reducers/favouritesReducer";
-import playersReducer from "../reducers/playersReducer";
-
-const rootReducer = combineReducers({
-  favorites: favouritesReducer,
-
-  player: playersReducer,
-});
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: favouritesReducer,
 });
